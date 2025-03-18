ef7ac1e246a18c572d24e2e22f3d6b0053830758
therealrajdeep <rajdeepjadeja2806@gmail.com>
2025-03-18 17:16:48 +0530

    "Removed Unnecessary Files"

--- backend/core/lottery/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import UserWallet,Lottery,Ticket  # Import the UserWallet model
-
-
-# Register your models here.
-admin.site.register(UserWallet)
-admin.site.register(Lottery)  # Register the Lottery model
-admin.site.register(Ticket)  # Register the Ticket model
